015cc88b43bd396af15b2ffe1b4b1b047e9e2783
eamondo2 <eamonbb@gmail.com>
2021-10-29 17:45:44 -0400

    working rg copyover

--- modules/godotnect/godotnect.cpp
+++ modules/godotnect/godotnect.cpp
@@ -13,9 +13,7 @@
 class MyFreenectDevice : public Freenect::FreenectDevice {
 public:
 	MyFreenectDevice(freenect_context *_ctx, int _index) :
-			Freenect::FreenectDevice(_ctx, _index),
-			m_buffer_video(freenect_find_video_mode(FREENECT_RESOLUTION_MEDIUM, FREENECT_VIDEO_RGB).bytes),
-			m_buffer_depth(freenect_find_depth_mode(FREENECT_RESOLUTION_MEDIUM, FREENECT_DEPTH_11BIT).bytes){
+			Freenect::FreenectDevice(_ctx, _index) {
 		setDepthFormat(FREENECT_DEPTH_11BIT);
 
 		//Initialize holding buffers for depth and video data
@@ -23,13 +21,17 @@ public:
 		video_data.resize(freenect_find_video_mode(FREENECT_RESOLUTION_MEDIUM, FREENECT_VIDEO_RGB).bytes);
 	}
 
+	//Handles passing the feed object into the MyFreenectDevice instance. This is so that for a single kinect we can have
+	//two streams, one of depth data and one for video data.
 	void initForFeed(CameraFeed *p_feed, int p_idx) {
-
-		switch (p_idx) { 
+		//Switch on the given idx value. 0 for depth, 1 for video
+		switch (p_idx) {
+			//depth init, set the feed_depth object and set active to true.
 			case 0:
 				feed_depth = p_feed;
 				depth_active = true;
 				break;
+			//video init, set the feed_video object and set active to true
 			case 1:
 				feed_video = p_feed;
 				video_active = true;
@@ -41,69 +43,76 @@ public:
 
 	// Do not call directly, even in child
 	void VideoCallback(void *_rgb, uint32_t timestamp) {
+		//video data comes in asa uint8_t array.
 		uint8_t *rgb = static_cast<uint8_t *>(_rgb);
-
+		//get the write pointer to the Godot internal Vector
 		uint8_t *w = video_data.ptrw();
 
-		//This will almost certainly not work
 
+		//We need to check if this feed has activated the video stream from the kinect, as well as if the feed object itself
+		//registers as active.
+		//This prevents the callback from attempting to write to null image objects, as the freenect side of things gets running faster 
+		//than the game engine can get to a ready state to instantiate objects.
 		if (video_active && feed_video->is_active()) {
 			
-			memcpy(w, rgb, getVideoBufferSize());
-			
+			//Copy the data over from the buffer			
+			memcpy(w, rgb, video_data.size());
+			//Create new refcounted image object
 			Ref<Image> img;
-
+			//Instantiate the image
 			img.instantiate();
+			//Create the image, give it the dimensions and the format and the data buffer
 			img->create(640, 480, false, Image::FORMAT_RGB8, video_data);
-
+			//set the feed's RGB stream 
 			feed_video->set_RGB_img(img);
 		}
-
-
 	}
 
 	// Do not call directly, even in child
 	void DepthCallback(void *_depth, uint32_t timestamp) {
-		
+		//Depth data comes in as a uint16_t array from the kinect.
 		uint16_t *depth = static_cast<uint16_t *>(_depth);
-
+		//The image object will only accept depth data vectors based on uint8_t.
 		uint8_t *w = depth_data.ptrw();
 
-		//This also will almost cetainly not work
-
-		//New image as dest for depth data
-
+		//We need to check if this feed has activated the depth stream from the kinect, as well as if the feed object itself
+		//registers as active.
+		//This prevents the callback from attempting to write to null image objects, as the freenect side of things gets running faster 
+		//than the game engine can get to a ready state to instantiate objects.
 		if (depth_active && feed_depth->is_active()) {
+			//testing copy to rgb space
+			//Since we're going from a uint16_t to a uint8_t space, we write two locations for the R and G data
+			//We technically could also get B data as well, or calculate it, but it won't exist in the inbound stream.
+			//TODO: add a calc and value set for the B channel to indicate IR shadow maybe?
+			//If there's a way to get the kinect's output to give us that information, we can perhaps set a flag value.
+			for (int i = 0; i < 640*480; i++) {
+				w[2 * i + 0] = depth[i] >> 8;
+				w[2 * i + 1] = depth[i] & 0xff;
+			}
 
-			
-			
-			memcpy(w, depth, getDepthBufferSize());
-			
+			//New image as dest for depth data
 			Ref<Image> img;
-
+			//Need to instantiate the image before creation, as it is a ref object.
 			img.instantiate();
+			//Create the image, passing resolution and the format, and the depth data.
 			img->create(640, 480, false, Image::FORMAT_RG8, depth_data);
-
+			//Set the feed's RGB image to the image created.
 			feed_depth->set_RGB_img(img);
 		}
 	}
 
+	//Create two generic CameraFeed objects to hold pointers back to the two separate feeds we need.
 	CameraFeed *feed_depth;
 	CameraFeed *feed_video;
 
 private:
-
-	std::vector<uint8_t> m_buffer_video;
-
-	std::vector<uint16_t> m_buffer_depth;
-
+	//Godot internal Vector types for depth and video data.
 	Vector<uint8_t> depth_data;
-
 	Vector<uint8_t> video_data;
 
+	//Flags to determine whether the feeds have been initialized yet.
 	bool depth_active = false;
 	bool video_active = false;
-
 };
 
 ///////////////////////////////////////////////////////
@@ -112,7 +121,9 @@ private:
 class GodotNectFeed : public CameraFeed {
 private:
 	MyFreenectDevice *device;
+
 public:
+	//The index for this feed, 0 is depth 1 is video
 	int feed_idx;
 	GodotNectFeed();
 	~GodotNectFeed();
@@ -121,6 +132,7 @@ public:
 
 	MyFreenectDevice *get_device() const;
 
+	//Passes the MyFreenectDevice object in, and sets it.
 	void set_device(MyFreenectDevice *device, int idx);
 
 	void deactivate_feed();
@@ -136,15 +148,15 @@ MyFreenectDevice *GodotNectFeed::get_device() const {
 //Allow for setting the device externally
 //idx indicates video or depth info, idx = 0 means depth, idx = 1 means video
 void GodotNectFeed::set_device(MyFreenectDevice *p_device, int idx) {
-
 	feed_idx = idx;
 	device = p_device;
 };
 
+//This probably wants to be doing more things? Not sure if this is necessary.
 GodotNectFeed::GodotNectFeed() {
 	device = nullptr;
 };
-
+//Not sure if this needs more cleanup steps here or not.
 GodotNectFeed::~GodotNectFeed(){
 
 };
@@ -153,11 +165,10 @@ bool GodotNectFeed::activate_feed() {
 	//Need to query list of available devices, eventually we might have more than one kinect available
 
 	//For now we can assume that there will only be the one at index 0
-
+	//Pass the feed object into the MyFreenectDevice instance, and give it the required index.
 	device->initForFeed(this, this->feed_idx);
-
-
-	switch ( feed_idx ) {
+	//Switch on the feed index, and start the respective stream from the kinect.
+	switch (feed_idx) {
 		case 0:
 			device->startDepth();
 			break;
@@ -174,9 +185,7 @@ bool GodotNectFeed::activate_feed() {
 
 void GodotNectFeed::deactivate_feed() {
 	//Need more for clean shutdown handling
-	// device->stopVideo();
-	// device->stopDepth();
-
+	//Switch on feed_idx, stop respective stream from kinect.
 	switch (feed_idx) {
 		case 0:
 			device->stopDepth();
@@ -184,7 +193,7 @@ void GodotNectFeed::deactivate_feed() {
 		case 1:
 			device->stopVideo();
 			break;
-		default: 
+		default:
 			print_line("This should not happen, wrong feed idx deactivate");
 			break;
 	}
@@ -193,46 +202,47 @@ void GodotNectFeed::deactivate_feed() {
 ///////////////////////////////////////////////////////////
 // Subclass of CameraServer
 
+//This creates and instantiates the feeds presented from the kinect, and manages them with the server.
+//This will need overhaul to manage multiple kinect devices, but that will require modifying the libfreenect library itself
+//Because currently we cannot get access to low-level serial info from the c++ wrapper side of things.
+//Technically, the implementation works and will handle multiple devices, but requires giving it an ID out of the list generated by deviceCount. 
+//This is not useful because it doesn't guarantee the ID will be static or give identifying info.
 void GodotNect::update_feeds() {
 	int num_devices = freenect.deviceCount();
 
 	//For now we just walk the list of devices that exist and initialize each as a feeed.
 	for (int i = 0; i < num_devices; i++) {
-		
-		//Create depth feed
 
+		//Create depth feed
 		Ref<GodotNectFeed> newfeed_depth;
-		
+		//Instantiate, as it is a ref object
 		newfeed_depth.instantiate();
 
 		//Create and set the device
 		newfeed_depth->set_device(&freenect.createDevice<MyFreenectDevice>(i), 0);
-
+		//Give it a friendly name
 		newfeed_depth->set_name("Kinect_depth");
-
+		//Add the feed
 		add_feed(newfeed_depth);
 
 		//Create video feed
-
 		Ref<GodotNectFeed> newfeed_video;
-
+		//Instantiate, as it is a ref object
 		newfeed_video.instantiate();
-
+		//We don't need to create a new MyFreenectDevice here, just pass the one already tied to the newfeed_depth object.
 		newfeed_video->set_device(newfeed_depth->get_device(), 1);
-
+		//Give it a friendly name
 		newfeed_video->set_name("Kinect_video");
-
+		//Add the feed.
 		add_feed(newfeed_video);
-
-
-
 	};
 };
 
+//Calls update_feeds to add the feeds to the main listing.
 GodotNect::GodotNect() {
 	update_feeds();
 };
-
+//Currently does nothing.
 GodotNect::~GodotNect(){
 
 };
